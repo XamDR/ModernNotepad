a804a64df751b1a64d79756547fc41087d547fd9
Max DR <maxcharter.dr@outlook.com.pe>
2020-05-20 22:39:03 -0500

    - Added a splash screen. - Revamped icon. - Localization completed for the settings window.

--- ModernNotepad/Program.cs
+++ ModernNotepad/Program.cs
@@ -4,6 +4,7 @@ using ModernNotepadLibrary.Core;
 using ModernNotepadLibrary.Helpers;
 using ModernNotepadLibrary.Services;
 using System;
+using System.Windows;
 
 namespace ModernNotepad
 {
@@ -14,18 +15,30 @@ namespace ModernNotepad
         [STAThread]
         public static void Main()
         {
+            ShowSplashScreen();           
             var app = new App();
-            app.InitializeComponent();
+            app.InitializeComponent();            
+            RegisterServices();
+            app.Run();
+        }
+
+        private static void ShowSplashScreen()
+        {
+            var splashScreen = new SplashScreen("Images/SplashScreen.png");
+            splashScreen.Show(true);
+        }
+
+        private static void RegisterServices()
+        {
             ServiceResolver = new ServiceResolver();
             ServiceResolver.Register<IApplicationThemeManager, ApplicationThemeManager>();
             ServiceResolver.Register<IContentDialogService, ContentDialogService>();
             ServiceResolver.Register<ILocaleManager, LocaleManager>();
             ServiceResolver.Register<IOpenFileService, OpenFileDialogService>();
+            ServiceResolver.Register<IPrintService, PrintService>();
             ServiceResolver.Register<ISaveFileService, SaveFileDialogService>();
             ServiceResolver.Register<ISettingsManager<UserSettings>, SettingsManager>();
-            ServiceResolver.Register<IWindowService, WindowService>();
-            ServiceResolver.Register<IPrintService, PrintService>();
-            app.Run();
+            ServiceResolver.Register<IWindowService, WindowService>();            
         }
     }
 }
